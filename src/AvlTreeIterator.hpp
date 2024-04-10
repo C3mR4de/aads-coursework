ba46add9654a57b1998e62f7c86717b841d15d8d
C3mR4de <NVadim2005@gmail.com>
2024-04-10 22:49:34 +0300

    Added basic implementation for AvlTreeIterator

--- src/AvlTreeIterator.hpp
+++ src/AvlTreeIterator.hpp
@@ -32,14 +32,102 @@ namespace coursework
         T& operator->();
         const T& operator->() const;
 
-        bool operator==(const AvlTreeIterator& other);
-        bool operator!=(const AvlTreeIterator& other);
+        bool operator==(const AvlTreeIterator& other) const;
+        bool operator!=(const AvlTreeIterator& other) const;
 
     private:
 
+        const Node<T>* root_;
         Node<T>* node_;
         explicit AvlTreeIterator(Node<T>* node);
     };
+}
+
+template <typename T>
+coursework::AvlTreeIterator<T>::AvlTreeIterator():
+    root_(nullptr),
+    node_(nullptr)
+{}
+
+template <typename T>
+coursework::AvlTreeIterator<T>::AvlTreeIterator(Node<T>* node):
+    root_(node),
+    node_(node)
+{
+    while (root_->parent_ != nullptr)
+    {
+        root_ = root_->parent_;
+    }
+}
+
+template <typename T>
+coursework::AvlTreeIterator<T>& coursework::AvlTreeIterator<T>::operator++()
+{
+    Node<T>* curr = this;
+
+    while (curr->right_ == nullptr || curr->key_ < root_->key_)
+    {
+        if (curr == root_ && (root_->right_ == nullptr || curr->key_ < root_->key_))
+        {
+            return nullptr;
+        }
+
+        curr = curr->parent_;
+
+        if (curr->key_ > root_->key_)
+        {
+            return curr;
+        }
+    }
+
+    curr = curr->right_;
+    curr = curr->begin();
+
+    return curr;
+}
+
+template <typename T>
+coursework::AvlTreeIterator<T>& coursework::AvlTreeIterator<T>::operator++(int)
+{
+    Node<T>* temp = AvlTreeIterator<T>(*this);
+    operator++();
+    return temp;
+}
+
+template <typename T>
+T& coursework::AvlTreeIterator<T>::operator*()
+{
+    return node_->key_;
+}
+
+template <typename T>
+const T& coursework::AvlTreeIterator<T>::operator*() const
+{
+    return node_->key_;
+}
+
+template <typename T>
+T& coursework::AvlTreeIterator<T>::operator->()
+{
+    return node_->key;
+}
+
+template <typename T>
+const T& coursework::AvlTreeIterator<T>::operator->() const
+{
+    return node_->key;
+}
+
+template <typename T>
+bool coursework::AvlTreeIterator<T>::operator==(const AvlTreeIterator& other) const
+{
+    return root_ == other.root_ && node_ == other.node_;
+}
+
+template <typename T>
+bool coursework::AvlTreeIterator<T>::operator!=(const AvlTreeIterator& other) const
+{
+    return root_ != other.root_ || node_ != other.node_;
 }
 
 #endif // AVL_TREE_ITERATOR_HPP
