cfa2190c75129fbb6a37cfb13a1f3a10af60b1e9
C3mR4de <NVadim2005@gmail.com>
2024-04-10 23:25:22 +0300

    Added basic implementation for AvlTreeConstIterator class

--- src/AvlTreeConstIterator.hpp
+++ src/AvlTreeConstIterator.hpp
@@ -20,23 +20,99 @@ namespace coursework
         AvlTreeConstIterator(const AvlTreeConstIterator&) = default;
         ~AvlTreeConstIterator() = default;
 
-        AvlTreeConstIterator& operator=(const AvlTreeConstIterator&) = default;
-        AvlTreeConstIterator& operator++();
-        AvlTreeConstIterator& operator++(int);
-        AvlTreeConstIterator& operator--();
-        AvlTreeConstIterator& operator--(int);
+        const AvlTreeConstIterator& operator=(const AvlTreeConstIterator&) = default;
+        const AvlTreeConstIterator& operator++();
+        const AvlTreeConstIterator& operator++(int);
+        const AvlTreeConstIterator& operator--();
+        const AvlTreeConstIterator& operator--(int);
 
         const T& operator*() const;
         const T& operator->() const;
 
-        bool operator==(const AvlTreeConstIterator& other);
-        bool operator!=(const AvlTreeConstIterator& other);
+        bool operator==(const AvlTreeConstIterator& other) const;
+        bool operator!=(const AvlTreeConstIterator& other) const;
 
     private:
 
+        const Node<T>* root_;
         const Node<T>* node_;
-        explicit AvlTreeConstIterator(Node<T>* node);
+        explicit AvlTreeConstIterator(const Node<T>* node);
     };
+}
+
+template <typename T>
+coursework::AvlTreeConstIterator<T>::AvlTreeConstIterator():
+    root_(nullptr),
+    node_(nullptr)
+{}
+
+template <typename T>
+coursework::AvlTreeConstIterator<T>::AvlTreeConstIterator(const Node<T>* node):
+    root_(node),
+    node_(node)
+{
+    while (root_->parent_ != nullptr)
+    {
+        root_ = root_->parent_;
+    }
+}
+
+template <typename T>
+const coursework::AvlTreeConstIterator<T>& coursework::AvlTreeConstIterator<T>::operator++()
+{
+    Node<T>* curr = this;
+
+    while (curr->right_ == nullptr || curr->key_ < root_->key_)
+    {
+        if (curr == root_ && (root_->right_ == nullptr || curr->key_ < root_->key_))
+        {
+            return nullptr;
+        }
+
+        curr = curr->parent_;
+
+        if (curr->key_ > root_->key_)
+        {
+            return curr;
+        }
+    }
+
+    curr = curr->right_;
+    curr = curr->begin();
+
+    return curr;
+}
+
+template <typename T>
+const coursework::AvlTreeConstIterator<T>& coursework::AvlTreeConstIterator<T>::operator++(int)
+{
+    Node<T>* temp = AvlTreeConstIterator<T>(*this);
+    operator++();
+    return temp;
+}
+
+template <typename T>
+const T& coursework::AvlTreeConstIterator<T>::operator*() const
+{
+    return node_->key_;
+}
+
+template <typename T>
+const T& coursework::AvlTreeConstIterator<T>::operator->() const
+{
+    return node_->key;
+}
+
+template <typename T>
+bool coursework::AvlTreeConstIterator<T>::operator==(const AvlTreeConstIterator& other) const
+{
+    return root_ == other.root_ && node_ == other.node_;
+}
+
+template <typename T>
+bool coursework::AvlTreeConstIterator<T>::operator!=(const AvlTreeConstIterator& other) const
+{
+    return root_ != other.root_ || node_ != other.node_;
 }
 
 #endif // AVL_TREE_CONST_ITERATOR_HPP
