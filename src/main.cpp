0f83d267d247dfe3224c8f4f9d072ef6064f74d5
C3mR4de <NVadim2005@gmail.com>
2024-04-21 15:22:52 +0300

    Added simple example in main.cpp file

--- src/main.cpp
+++ src/main.cpp
@@ -4,6 +4,19 @@
 int main()
 {
     coursework::AvlTree<int> tree;
+
+    tree.insert(5);
+    tree.insert(4);
+    tree.insert(-1000);
+    tree.insert(9);
+    tree.insert(8);
+
+    for (const auto& i : tree)
+    {
+        std::cout << i << '\n';
+    }
+
     std::cout << "Hello World\n";
+
     return 0;
 }
